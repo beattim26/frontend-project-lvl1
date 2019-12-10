ad2eef50ebef7ce5223473207939a218ed45d3e9
beattim26 <beattimy@yandex.ru>
2019-12-10 14:38:24 +0300

    fix refactoring 7

--- src/engine.js
+++ src/engine.js
@@ -3,29 +3,17 @@ import readlineSync from 'readline-sync';
 const quantityQuestions = 3;
 
 const startGame = (makeQuestion, message) => {
-  const iter = (func, userName, acc = 0) => {
-    if (acc > quantityQuestions) {
-      console.log(`Congratulations, ${userName}!`);
-      return null;
-    }
-
-    if (acc < 1) {
-      console.log('Welcome to the Brain Games!');
-      console.log(message);
-      console.log('');
-      const name = readlineSync.question('May I have your name?');
-      console.log(`Hello, ${name}!`);
-      console.log('');
-
-      return iter(func, name, 1);
-    }
-
+  const iter = (func, userName, acc) => {
     const question = makeQuestion();
     console.log(`Question: ${question[0]}`);
     const userAnswer = readlineSync.question('Your answer: ');
     // user answer === correct answer, call function again
     if (userAnswer.toLowerCase() === question[1]) {
       console.log('Correct!');
+      if (acc === quantityQuestions) {
+        console.log(`Congratulations, ${userName}!`);
+        return null;
+      }
       return iter(func, userName, acc + 1);
     }
 
@@ -34,7 +22,14 @@ const startGame = (makeQuestion, message) => {
     return iter(func, userName, 1);
   };
 
-  iter(makeQuestion);
+  console.log('Welcome to the Brain Games!');
+  console.log(message);
+  console.log('');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${name}!`);
+  console.log('');
+
+  iter(makeQuestion, name, 1);
 };
 
 export default startGame;
